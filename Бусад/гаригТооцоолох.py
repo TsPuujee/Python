dfd19fc34c9c953a1ba88c7998f7a71d15184797
Пүрэвжаргал <puujeep60@gmail.com>
2018-08-16 10:37:44 +0800

    Тайлбар оруулав

--- Бусад/гаригТооцоолох.py
+++ Бусад/гаригТооцоолох.py
@@ -1,4 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 16 10:34:10 2018
+
+@author: geeks
+
+Tomohiko Sakamoto’s Algorithm
+"""
+
 def dow(y, m, d):
-    t = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4]
-    y -= m<3
-    return (y + y//4 - y//100 + y//400 + t[m-1] + d) % 7
+    t = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4] #тогтмол
+    y -= m<3 # сар нь 3аас бага байвал 1-ийг хасна.
+    return (y + y//4 - y//100 + y//400 + t[m-1] + d) % 7 # python // хуваах үйлдэл нь бүхлээр хуваах c хэлний / үйлдэлтэй адил
+
+day = 18
+month = 8
+year = 2018
+week_days = ["Ням", "Даваа", "Мягмар", "Лхагва", "Пүрэв", "Баасан", "Бямба"]
+print(week_days[dow(year, month, day)]);
