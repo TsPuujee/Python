b62a3477683fcc97446e2b02171e3c589dd1bc85
Пүрэвжаргал <puujeep60@gmail.com>
2019-05-29 18:16:50 +0800

    Create helloWorld.py

---
+++ helloWorld.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 19 09:53:36 2018
+@author: Geek
+
+"""
+    
+###############################
+# Жишээ: Hello world
+###############################
+
+print("Hello, World!")
